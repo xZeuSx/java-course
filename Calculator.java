a8ea757eae8d56b75c144f754c645ec742c9873d
xZeuSx <paycry@gmail.com>
2015-07-27 00:02:59 +0600

    Калькулятор basic

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator{
+	
+	//Результат вычисления
+	private int result;
+	
+	//Суммируем аргументы
+	public void add(int ... params){
+		for(Integer param : params){
+			this.result += param;
+		}
+	}
+	
+	//Получить результат
+	public int getResult(){
+		return this.result;
+	}
+	
+	//Очистить результат
+	public void cleanResult(){
+		this.result = 0;
+	}
+}
