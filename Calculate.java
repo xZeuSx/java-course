d86b45b6582ad8ae12d05ca61a378c1bcc58a4c3
xZeuSx <paycry@gmail.com>
2015-07-26 23:04:32 +0600

    double c = 2.5

--- Calculate.java
+++ Calculate.java
@@ -3,6 +3,8 @@ public class Calculate{
     private int a = 5;
     private int b = 10;
 
+    private double c = 2.5;
+
 	public static void main(String[] args){
 		System.out.println("Calculate......");
 	}
